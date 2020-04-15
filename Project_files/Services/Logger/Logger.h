bde37961b0975c8669d92463b46e73efdd44f1e0
mustafaBahaa123 <mustafa.bahaa123@gmail.com>
2020-04-15 03:53:36 +0200

    ADDED Logger , still not implemnted yet

---
+++ Project_files/Services/Logger/Logger.h
@@ -0,0 +1,64 @@
+/****************************************************************************
+Copyright (c) 2020 [Mustafa Bahaa]
+*****************************************************************************
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Softwar without restriction,  including without  limitation the rights
+to use, copy, modify,  merge,  publish,  distribute,  sublicense, and/or sell
+copies  of  the  Software,  and to  permit  persons  to  whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+*****************************************************************************/
+#ifndef _LOGGER_H_
+#define _LOGGER_H_
+/****************************************************************************/
+/*                               Include Files                              */
+/****************************************************************************/
+#include "Logger_config.h"
+/****************************************************************************/
+/*                               Public Functions                           */
+/****************************************************************************/
+#ifdef WARNING
+/**
+	* @brief  print messages based on uart , warning only
+	* @param  msg  : char array to be printed
+	* @retval void 
+	*/
+void logger_print_warning (char * msg );
+#endif  /*WARNING*/
+
+
+#ifdef ERROR
+/**
+	* @brief  print messages based on uart , Error only
+	* @param  msg  : char array to be printed
+	* @retval void 
+	*/
+void logger_print_error (char * msg );
+#endif /*ERROR*/
+
+
+#ifdef DEBUG
+/**
+	* @brief  print messages based on uart , debug only
+	* @param  msg  : char array to be printed
+	* @retval void 
+	*/
+void logger_print_debug (char * msg );
+#endif /*DEBUG*/
+
+
+#ifdef INFO
+/**
+	* @brief  print messages based on uart , info only
+	* @param  msg  : char array to be printed
+	* @retval void 
+	*/
+void logger_print_info (char * msg );
+#endif /*INFO*/
+
+#endif /*_LOGGER_H_*/
+
+/****************************************************************************/
---
+++ Project_files/Services/Logger/Logger_config.h
@@ -0,0 +1,27 @@
+/****************************************************************************
+Copyright (c) 2020 [Mustafa Bahaa]
+*****************************************************************************
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Softwar without restriction,  including without  limitation the rights
+to use, copy, modify,  merge,  publish,  distribute,  sublicense, and/or sell
+copies  of  the  Software,  and to  permit  persons  to  whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+*****************************************************************************/
+#ifndef _LOGGER_CONFIG_H_
+#define _LOGGER_CONFIG_H_
+
+/****************************************************************************/
+/*                               Configurations                             */
+/****************************************************************************/
+#define WARNING   
+#define DEBUG    
+#define INFO      
+#define ERROR     
+
+#endif /*_LOGGER_CONFIG_H_*/
+
+/****************************************************************************/
